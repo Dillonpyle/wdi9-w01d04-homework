f0681d012c7b9d3996b7a8b2b5ebeaafcf13250a
Computer <computer@Computers-MacBook-Pro.local>
2018-12-12 20:14:32 -0700

    Digit sum

--- 12_13_18_homework/js/app.js
+++ 12_13_18_homework/js/app.js
@@ -13,3 +13,16 @@ console.log("working");
 //return saves the value
 
 // Palindrome again
+function checkPalindrome(s, i) {
+    return (i = i || 0) < 0 || i >= s.length >> 1 || s[i] == s[s.length - 1 - i] && checkPalindrome(s, ++i);
+}
+
+//console.log(checkPalindrome('racecar'));
+
+//console.log(checkPalindrome('borscht'));
+
+//digit sum
+//const n = 42;
+//let newArr = Array.from(n.toString()).map(Number);
+//let sumOfArray = newArr[0] + newArr[1];
+//console.log(sumOfArray);

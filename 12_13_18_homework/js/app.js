3b8acf8a835bd0196b652ddb0e7ce3d95dc49a0d
Computer <computer@Computers-MacBook-Pro.local>
2018-12-12 17:58:22 -0700

    verbal questions

---
+++ 12_13_18_homework/js/app.js
@@ -0,0 +1,15 @@
+console.log("working");
+
+// Verbal questions
+
+//1
+
+// a parameter is the variable in a method and the argument is the data you pass into the method
+
+//2
+//return stops the code and returns a value and console.log will not influence the flow of your code.
+
+//3
+//return saves the value
+
+// Palindrome again

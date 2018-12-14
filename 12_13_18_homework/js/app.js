c149e17b45ea531e89d15f9f6468e2d84585b885
Computer <computer@Computers-MacBook-Pro.local>
2018-12-14 10:53:19 -0700

    done

--- 12_13_18_homework/js/app.js
+++ 12_13_18_homework/js/app.js
@@ -3,7 +3,6 @@ console.log("working");
 // Verbal questions
 
 //1
-
 // a parameter is the variable in a method and the argument is the data you pass into the method
 
 //2
@@ -13,9 +12,9 @@ console.log("working");
 //return saves the value
 
 // Palindrome again
-function checkPalindrome(s, i) {
-    return (i = i || 0) < 0 || i >= s.length >> 1 || s[i] == s[s.length - 1 - i] && checkPalindrome(s, ++i);
-}
+//function checkPalindrome(s, i) {
+//  return (i = i || 0) < 0 || i >= s.length >> 1 || s[i] == s[s.length - 1 - i] && checkPalindrome(s, ++i);
+//}
 
 //console.log(checkPalindrome('racecar'));
 
@@ -28,12 +27,49 @@ function checkPalindrome(s, i) {
 //console.log(sumOfArray);
 
 // Pythagoras
-let a = 8
-let b = 6
 
 
-function calculateSide(a, b) {
-    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//function calculateSide(a, b) {
+//    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//}
+
+//console.log(calculateSide(a, b));
+
+//Prime numbers
+
+
+
+//const isPrime = (check) => {
+//    for (let i = 2; i <= Math.sqrt(check); i++) {
+//        if (check % i == 0) {
+//            return false;
+//        } else if (i == Math.floor(Math.sqrt(check))) {
+//            return true;
+//        }
+//    }
+//}
+
+//console.log(isPrime(7));
+
+const checkPrime = (check) => {
+    let primes = [];
+    for (let i = 2; i < check; i++) {
+        primes[i] = true;
+    }
+    let limit = Math.sqrt(check);
+    for (let i = 2; i < limit; i++) {
+        if (primes[i] === true) {
+            for (let j = i * i; j < check; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    for (let i = 2; i < check; i++) {
+        if (primes[i] === true) {
+            console.log(i + " " + primes[i]);
+        }
+    }
 }
 
-console.log(calculateSide(a, b));
+
+console.log(checkPrime(97));
